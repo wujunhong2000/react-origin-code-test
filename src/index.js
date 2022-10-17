d8c5e3f22ef06bec9c3ad24be241d2ece56b9864
wujunhong <wb.wujunhong01@mesg.corp.netease.com>
2022-10-17 20:36:50 +0800

    first

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const ele = (
+  <div>
+    <h3 className="title">hello</h3>
+  </div>
+);
+
+ReactDOM.render(ele, document.querySelector('#root'));
